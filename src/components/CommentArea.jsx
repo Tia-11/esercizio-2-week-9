298fd9b0a72b21f1a3f0555cb73716b892f5f34b
Tia-11 <143324169+Tia-11@users.noreply.github.com>
2023-10-31 16:13:49 +0100

    nuovo

--- src/components/CommentArea.jsx
+++ src/components/CommentArea.jsx
@@ -1,85 +1,55 @@
-import { Component } from 'react'
-import CommentList from './CommentList'
-import AddComment from './AddComment'
-import Loading from './Loading'
-import Error from './Error'
+import React, { useState, useEffect, useCallback } from 'react';
+import CommentList from './CommentList';
+import AddComment from './AddComment';
+import Loading from './Loading';
+import Error from './Error';
 
-class CommentArea extends Component {
-  state = {
-    comments: [],
-    isLoading: false,
-    isError: false,
-  }
+function CommentArea(props) {
+  const [comments, setComments] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
-  // componentDidMount = async () => {
-  //   try {
-  //     let response = await fetch(
-  //       'https://striveschool-api.herokuapp.com/api/comments/' +
-  //         this.props.asin,
-  //       {
-  //         headers: {
-  //           Authorization:
-  //             'Bearer inserisci-qui-il-tuo-token',
-  //         },
-  //       }
-  //     )
-  //     console.log(response)
-  //     if (response.ok) {
-  //       let comments = await response.json()
-  //       this.setState({ comments: comments, isLoading: false, isError: false })
-  //     } else {
-  //       console.log('error')
-  //       this.setState({ isLoading: false, isError: true })
-  //     }
-  //   } catch (error) {
-  //     console.log(error)
-  //     this.setState({ isLoading: false, isError: true })
-  //   }
-  // }
-
-  componentDidUpdate = async (prevProps) => {
-    if (prevProps.asin !== this.props.asin) {
-      this.setState({
-        isLoading: true,
-      })
-      try {
-        let response = await fetch(
-          'https://striveschool-api.herokuapp.com/api/comments/' +
-            this.props.asin,
-          {
-            headers: {
-              Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTQxMDk1YmIxODE2MzAwMTRjOGZmODciLCJpYXQiOjE2OTg3NjEwNTEsImV4cCI6MTY5OTk3MDY1MX0.wGepSCkFaeJ0KSXyHomesrxE0uXnCp8ilmqfdJCKd2c",
-            },
-          }
-        )
-        console.log(response)
-        if (response.ok) {
-          let comments = await response.json()
-          this.setState({
-            comments: comments,
-            isLoading: false,
-            isError: false,
-          })
-        } else {
-          this.setState({ isLoading: false, isError: true })
+  const fetchComments = useCallback(async () => {
+    setIsLoading(true);
+    try {
+      let response = await fetch(
+        'https://striveschool-api.herokuapp.com/api/comments/' + props.asin,
+        {
+          headers: {
+            Authorization:  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTQxMDk1YmIxODE2MzAwMTRjOGZmODciLCJpYXQiOjE2OTg3NjEwNTEsImV4cCI6MTY5OTk3MDY1MX0.wGepSCkFaeJ0KSXyHomesrxE0uXnCp8ilmqfdJCKd2c",
+          },
         }
-      } catch (error) {
-        console.log(error)
-        this.setState({ isLoading: false, isError: true })
+      );
+      if (response.ok) {
+        let comments = await response.json();
+        setComments(comments);
+        setIsLoading(false);
+        setIsError(false);
+      } else {
+        setIsLoading(false);
+        setIsError(true);
       }
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+      setIsError(true);
     }
-  }
+  }, [props.asin]);
 
-  render() {
-    return (
-      <div className="text-center">
-        {this.state.isLoading && <Loading />}
-        {this.state.isError && <Error />}
-        <AddComment asin={this.props.asin} />
-        <CommentList commentsToShow={this.state.comments} />
-      </div>
-    )
-  }
+  useEffect(() => {
+    if (props.asin) {
+      fetchComments();
+    }
+  }, [props.asin, fetchComments]);
+
+  return (
+    <div className="text-center">
+      {isLoading && <Loading />}
+      {isError && <Error />}
+      <AddComment asin={props.asin} />
+      <CommentList commentsToShow={comments} />
+    </div>
+  );
 }
 
-export default CommentArea
+export default CommentArea;
--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,36 +1,29 @@
-import { Component } from 'react'
-import { Card } from 'react-bootstrap'
-// import CommentArea from './CommentArea'
+import React, { useState } from 'react';
+import { Card } from 'react-bootstrap';
 
-class SingleBook extends Component {
-  // state = {
-  //   selected: false,
-  // }
+function SingleBook(props) {
+  const [isSelected, setIsSelected] = useState(false);
 
-  render() {
-    return (
-      <>
-        <Card
-          // onClick={() => this.setState({ selected: !this.state.selected })}
-          onClick={() => this.props.changeSelectedBook(this.props.book.asin)}
-          style={{
-            border:
-              this.props.selectedBook === this.props.book.asin
-                ? '3px solid red'
-                : 'none',
-          }}
-        >
-          <Card.Img variant="top" src={this.props.book.img} />
-          <Card.Body>
-            <Card.Title style={{ color: 'black' }}>
-              {this.props.book.title}
-            </Card.Title>
-          </Card.Body>
-        </Card>
-        {/* {this.state.selected && <CommentArea asin={this.props.book.asin} />} */}
-      </>
-    )
-  }
+  const toggleSelection = () => {
+    setIsSelected(!isSelected);
+    props.changeSelectedBook(props.book.asin);
+  };
+
+  return (
+    <>
+      <Card
+        onClick={toggleSelection}
+        style={{
+          border: isSelected ? '3px solid red' : 'none',
+        }}
+      >
+        <Card.Img variant="top" src={props.book.img} />
+        <Card.Body>
+          <Card.Title style={{ color: 'black' }}>{props.book.title}</Card.Title>
+        </Card.Body>
+      </Card>
+    </>
+  );
 }
 
-export default SingleBook
+export default SingleBook;
